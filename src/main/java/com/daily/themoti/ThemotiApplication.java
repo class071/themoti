c5f3f96c9a9faf8bae8bae86648b8780d713b522
we4sley <galaxy138742@gmail.com>
2022-04-18 15:51:46 +0900

    chore: 프로젝트 셋팅

---
+++ src/main/java/com/daily/themoti/ThemotiApplication.java
@@ -0,0 +1,13 @@
+package com.daily.themoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemotiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemotiApplication.class, args);
+	}
+
+}
