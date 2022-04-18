5b81657338ed1517cd650d46bddcc7078b6ca668
we4sley <galaxy138742@gmail.com>
2022-04-18 15:51:46 +0900

    chore: 프로젝트 셋팅

---
+++ src/test/java/com/daily/themoti/ThemotiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.themoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemotiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
