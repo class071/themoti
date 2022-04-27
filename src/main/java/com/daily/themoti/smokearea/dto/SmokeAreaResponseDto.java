6d16bb89bb5617276a1a64f00c45036209e78d88
gnoesnooj <306yyy@naver.com>
2022-04-27 15:57:52 +0900

    feat : 흡연장 entity, dto 작성

---
+++ src/main/java/com/daily/themoti/smokearea/dto/SmokeAreaResponseDto.java
@@ -0,0 +1,16 @@
+package com.daily.themoti.smokearea.dto;
+
+import com.daily.themoti.smokearea.SmokeArea;
+import lombok.Getter;
+
+@Getter
+public class SmokeAreaResponseDto {
+
+    private String longitude; // 경도
+    private String latitude; // 위도
+
+    public SmokeAreaResponseDto(SmokeArea smokeArea){
+        this.longitude = smokeArea.getLongitude();
+        this.latitude = smokeArea.getLatitude();
+    }
+}
