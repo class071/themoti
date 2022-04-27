78e7d68931386e1467b016f45e9cbd4d21f94a5b
gnoesnooj <306yyy@naver.com>
2022-04-27 15:57:52 +0900

    feat : 흡연장 entity, dto 작성

---
+++ src/main/java/com/daily/themoti/smokearea/SmokeArea.java
@@ -0,0 +1,29 @@
+package com.daily.themoti.smokearea;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class SmokeArea {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column
+    private String longitude; // 경도
+
+    @Column
+    private String latitude; // 위도
+
+    @Builder
+    public SmokeArea(String longitude, String latitude){
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+}
