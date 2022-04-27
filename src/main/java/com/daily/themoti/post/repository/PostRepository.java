30a345b25a663fdcc6e0456fcddd7f38f4977b22
we4sley <galaxy138742@gmail.com>
2022-04-27 17:12:56 +0900

    feat: Post Repository 구현

---
+++ src/main/java/com/daily/themoti/post/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.daily.themoti.post.repository;
+
+import com.daily.themoti.post.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
