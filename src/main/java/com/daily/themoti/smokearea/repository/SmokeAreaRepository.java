2cfed781339e87fe6f783ecb20259416ba42b562
gnoesnooj <306yyy@naver.com>
2022-04-27 16:00:41 +0900

    feat : 흡연장 리포지토리 구현

---
+++ src/main/java/com/daily/themoti/smokearea/repository/SmokeAreaRepository.java
@@ -0,0 +1,10 @@
+package com.daily.themoti.smokearea.repository;
+
+import com.daily.themoti.smokearea.SmokeArea;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SmokeAreaRepository extends JpaRepository<SmokeArea, Long> {
+
+}
