06c8e408ae913b4879cb5c7a800e2304c0a1a778
we4sley <galaxy138742@gmail.com>
2022-05-12 16:30:10 +0900

    feat: Reply Controller 구현

---
+++ src/main/java/com/daily/themoti/community/controller/ReplyController.java
@@ -0,0 +1,37 @@
+package com.daily.themoti.community.controller;
+
+import com.daily.themoti.global.api.ApiResponse;
+import com.daily.themoti.community.dto.ReplyCreateRequestDto;
+import com.daily.themoti.community.dto.ReplyResponseDto;
+import com.daily.themoti.community.service.ReplyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ReplyController {
+
+    private final ReplyService replyService;
+
+    @PostMapping("/api/reply")
+    public ApiResponse<ReplyResponseDto> create(@Valid @RequestBody ReplyCreateRequestDto replyCreateRequestDto) {
+        ReplyResponseDto replyResponseDto = replyService.create(replyCreateRequestDto);
+        return ApiResponse.success(HttpStatus.CREATED, replyResponseDto);
+    }
+
+    @DeleteMapping("/api/reply/{replyId}")
+    public ApiResponse<ReplyResponseDto> delete(@PathVariable Long replyId) {
+        replyService.delete(replyId);
+        return ApiResponse.success(HttpStatus.OK, replyId);
+    }
+
+    @GetMapping("/api/reply/{postId}")
+    public ApiResponse<List<ReplyResponseDto>> readAll(@PathVariable Long postId) {
+        List<ReplyResponseDto> replyResponseDtos = replyService.readAll(postId);
+        return ApiResponse.success(HttpStatus.OK, replyResponseDtos);
+    }
+}
