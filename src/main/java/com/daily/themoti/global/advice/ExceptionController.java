bf6f2d2c65e930f8e6ce25708ba1f201794b979b
we4sley <galaxy138742@gmail.com>
2022-05-12 14:30:31 +0900

    feat: 예외 컨트롤러 구현

---
+++ src/main/java/com/daily/themoti/global/advice/ExceptionController.java
@@ -0,0 +1,26 @@
+package com.daily.themoti.global.advice;
+
+import com.daily.themoti.global.api.ApiResponse;
+import com.daily.themoti.global.exception.NoSuchPostExist;
+import com.daily.themoti.global.exception.NoSuchReplyExist;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static com.daily.themoti.community.constant.CommunityConstant.POST_NOT_EXIST;
+import static com.daily.themoti.community.constant.CommunityConstant.REPLY_NOT_EXIST;
+
+@RestControllerAdvice
+public class ExceptionController extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NoSuchPostExist.class)
+    public ApiResponse handleNoSuchPostExist(NoSuchPostExist e) {
+        return ApiResponse.error(HttpStatus.NOT_FOUND, POST_NOT_EXIST);
+    }
+
+    @ExceptionHandler(NoSuchReplyExist.class)
+    public ApiResponse handleNoSuchReplyExist(NoSuchReplyExist exist) {
+        return ApiResponse.error(HttpStatus.NOT_FOUND, REPLY_NOT_EXIST);
+    }
+}
