bf9b8db3d2742282cb27aaa8ab14ae3a93be8300
we4sley <galaxy138742@gmail.com>
2022-05-12 19:07:08 +0900

    feat: Reply Entity 생성

---
+++ src/main/java/com/daily/themoti/community/reply/domain/Reply.java
@@ -0,0 +1,33 @@
+package com.daily.themoti.community.reply.domain;
+
+import com.daily.themoti.community.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Reply extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long replyId;
+
+    @Column(length = 100, nullable = false)
+    private String replyContent;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Builder
+    public Reply(String replyContent, Long postId, Long userId) {
+        this.replyContent = replyContent;
+        this.postId = postId;
+        this.userId = userId;
+    }
+}
