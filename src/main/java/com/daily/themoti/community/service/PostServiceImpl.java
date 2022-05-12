40d1b95ac423aca19e10b7d55d3db7b772e3f96d
we4sley <galaxy138742@gmail.com>
2022-05-12 14:29:23 +0900

    feat: Post Service 구현

---
+++ src/main/java/com/daily/themoti/community/service/PostServiceImpl.java
@@ -0,0 +1,61 @@
+package com.daily.themoti.community.service;
+
+import com.daily.themoti.global.exception.NoSuchPostExist;
+import com.daily.themoti.community.domain.Post;
+import com.daily.themoti.community.dto.PostCreateRequestDto;
+import com.daily.themoti.community.dto.PostResponseDto;
+import com.daily.themoti.community.dto.PostUpdateRequestDto;
+import com.daily.themoti.community.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Transactional
+    @Override
+    public PostResponseDto create(PostCreateRequestDto postCreateRequestDto) {
+        Post newPost = postRepository.save(postCreateRequestDto.toEntity());
+        return new PostResponseDto(newPost);
+    }
+
+    @Override
+    public PostResponseDto readOne(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new NoSuchPostExist());
+        return new PostResponseDto(post);
+    }
+
+    @Transactional
+    @Override
+    public void delete(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new NoSuchPostExist());
+        postRepository.delete(post);
+    }
+
+    @Transactional
+    @Override
+    public PostResponseDto update(Long postId, PostUpdateRequestDto postUpdateRequestDto) {
+        Post updatePost = postRepository.findById(postId)
+                .orElseThrow(() -> new NoSuchPostExist());
+        updatePost.update(postUpdateRequestDto.getTitle(), postUpdateRequestDto.getContent());
+        return new PostResponseDto(updatePost);
+    }
+
+    @Override
+    public List<PostResponseDto> readAll() {
+        List<Post> posts = postRepository.findAll();
+        List<PostResponseDto> postResponseDtos = posts.stream()
+                .map(post -> new PostResponseDto(post))
+                .collect(Collectors.toList());
+        return postResponseDtos;
+    }
+}
