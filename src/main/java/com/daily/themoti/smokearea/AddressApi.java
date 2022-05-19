3a528dec2955686447aceb91db78ef35cd8bdb20
gnoesnooj <306yyy@naver.com>
2022-05-19 13:46:11 +0900

    feat : 다른 구역 추가

--- src/main/java/com/daily/themoti/smokearea/AddressApi.java
+++ src/main/java/com/daily/themoti/smokearea/AddressApi.java
@@ -7,7 +7,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AddressApi {
 
-    KWANGJIN("영업소소재지(도로 명)");
+    KWANGJIN("영업소소재지(도로 명)"),
+    JOONG("설치도로명주소"),
+    JOONGRANG("주소"),
+    DONGJAK("주소"),
+    SONGPA("도로명주소"),
+    SEODAEMOON("설치위치"),
+    DONGDAEMOON("설치위치");
 
     private final String storedAt;
 }
