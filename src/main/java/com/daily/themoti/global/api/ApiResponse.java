b325351406319305612e2059d153432a8f2f12ec
gnoesnooj <306yyy@naver.com>
2022-05-20 16:26:26 +0900

    refactor : ApiResponse 수정

--- src/main/java/com/daily/themoti/global/api/ApiResponse.java
+++ src/main/java/com/daily/themoti/global/api/ApiResponse.java
@@ -8,24 +8,15 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public class ApiResponse<T> {
 
-    private String code;
     private HttpStatus httpStatus;
     private String message;
     private T response;
 
-    public static <T> ApiResponse success(String code, HttpStatus httpStatus, String message, T response) {
-        return new ApiResponse<>(code, httpStatus, message, response);
-    }
-
     public static <T> ApiResponse success(HttpStatus httpStatus, T response) {
-        return new ApiResponse(null, httpStatus, null, response);
-    }
-
-    public static ApiResponse error(String code, HttpStatus httpStatus, String message) {
-        return new ApiResponse<>(code, httpStatus, message, null);
+        return new ApiResponse( httpStatus, null, response);
     }
 
     public static ApiResponse error(HttpStatus httpStatus, String message) {
-        return new ApiResponse<>(null, httpStatus, message, null);
+        return new ApiResponse<>( httpStatus, message, null);
     }
 }
