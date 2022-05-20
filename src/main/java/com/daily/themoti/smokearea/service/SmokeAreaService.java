7c6573f18d3457b0e5fad9dcde7de19c0bd8816a
gnoesnooj <306yyy@naver.com>
2022-05-20 10:28:25 +0900

    feat : 전체조회, 구역별 조회 기능 추가

--- src/main/java/com/daily/themoti/smokearea/service/SmokeAreaService.java
+++ src/main/java/com/daily/themoti/smokearea/service/SmokeAreaService.java
@@ -1,10 +1,11 @@
 package com.daily.themoti.smokearea.service;
 
-
-import com.daily.themoti.smokearea.dto.LoadAreaResponseDto;
-import com.daily.themoti.smokearea.dto.SaveAreaRequestDto;
 import com.daily.themoti.smokearea.dto.SmokeAreaResponseDto;
+
+import java.util.List;
 
 public interface SmokeAreaService {
 
+    List<SmokeAreaResponseDto> findAll();
+    List<SmokeAreaResponseDto> findByArea(long api);
 }
