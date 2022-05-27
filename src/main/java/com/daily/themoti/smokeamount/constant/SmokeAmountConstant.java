6e839a1209caa3cea30cbdf6c6beb00a9daa92f4
we4sley <galaxy138742@gmail.com>
2022-05-27 15:49:33 +0900

    feat: 흡연량 예외 관련 상수 추가

---
+++ src/main/java/com/daily/themoti/smokeamount/constant/SmokeAmountConstant.java
@@ -0,0 +1,8 @@
+package com.daily.themoti.smokeamount.constant;
+
+public class SmokeAmountConstant {
+
+    public static final String NOT_CORRECT_VALUE = "올바른 값이 아닙니다.";
+    public static final String NOT_ENOUGH_PARAM = "필요한 파라미터 값을 모두 입력하시오.";
+    public static final String NOT_CORRECT_PARAM_TYPE = "파라미터의 자료형이 일치하지 않습니다.";
+}
