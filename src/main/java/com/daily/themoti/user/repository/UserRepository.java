cde55eea298a01004c2e61097342e59f74364099
gnoesnooj <306yyy@naver.com>
2022-05-31 14:57:24 +0900

    feat : user repository 구현 / email 조회 기능 구현

---
+++ src/main/java/com/daily/themoti/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.daily.themoti.user.repository;
+
+import com.daily.themoti.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
