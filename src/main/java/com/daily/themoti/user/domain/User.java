44ed3e499d79e28a7e3af0912e718169593a53db
gnoesnooj <306yyy@naver.com>
2022-05-31 14:58:24 +0900

    feat : user entity 구현

---
+++ src/main/java/com/daily/themoti/user/domain/User.java
@@ -0,0 +1,34 @@
+package com.daily.themoti.user.domain;
+
+import com.daily.themoti.user.constant.UserRole;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Builder
+    public User(String username, String email, UserRole role){
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
+}
