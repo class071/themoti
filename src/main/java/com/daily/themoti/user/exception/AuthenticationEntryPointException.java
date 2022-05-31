a8116c2536c397f13c460dcf976e9eb9c2cc225b
gnoesnooj <306yyy@naver.com>
2022-06-01 02:18:43 +0900

    feat : AuthenticationEntryPointException 예외 구현

---
+++ src/main/java/com/daily/themoti/user/exception/AuthenticationEntryPointException.java
@@ -0,0 +1,8 @@
+package com.daily.themoti.user.exception;
+
+public class AuthenticationEntryPointException extends RuntimeException {
+
+    public AuthenticationEntryPointException(String message){
+        super(message);
+    }
+}
