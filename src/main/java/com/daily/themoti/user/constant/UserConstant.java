31b7eef10321239ced4c79629920311b1690d3d0
gnoesnooj <306yyy@naver.com>
2022-06-02 15:58:30 +0900

    refactor : user service 예외 컨트롤러 처리

---
+++ src/main/java/com/daily/themoti/user/constant/UserConstant.java
@@ -0,0 +1,7 @@
+package com.daily.themoti.user.constant;
+
+public class UserConstant {
+
+    public static final String USER_NOT_EXIST = "User not exists";
+    public static final String UNAUTHORIZED = "User not authorized";
+}
