905e206ff5d3e0b32a9baf4b81491549090e6a79
gnoesnooj <306yyy@naver.com>
2022-06-02 15:58:30 +0900

    refactor : user service 예외 컨트롤러 처리

--- src/main/java/com/daily/themoti/global/advice/ExceptionController.java
+++ src/main/java/com/daily/themoti/global/advice/ExceptionController.java
@@ -3,6 +3,8 @@ package com.daily.themoti.global.advice;
 import com.daily.themoti.global.api.ApiResponse;
 import com.daily.themoti.community.exception.NoSuchPostExist;
 import com.daily.themoti.community.exception.NoSuchReplyExist;
+import com.daily.themoti.user.exception.UnauthorizedUserException;
+import com.daily.themoti.user.exception.UserNotExistException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +16,8 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import static com.daily.themoti.community.constant.CommunityConstant.POST_NOT_EXIST;
 import static com.daily.themoti.community.constant.CommunityConstant.REPLY_NOT_EXIST;
 import static com.daily.themoti.smokeamount.constant.SmokeAmountConstant.*;
+import static com.daily.themoti.user.constant.UserConstant.UNAUTHORIZED;
+import static com.daily.themoti.user.constant.UserConstant.USER_NOT_EXIST;
 
 @Slf4j
 @RestControllerAdvice
@@ -47,5 +51,17 @@ public class ExceptionController {
     public ApiResponse handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
         log.error(e.getMessage());
         return ApiResponse.error(HttpStatus.BAD_REQUEST, NOT_CORRECT_PARAM_TYPE);
+    }
+
+    @ExceptionHandler(UnauthorizedUserException.class)
+    public ApiResponse handleUnauthorizedUserException(UnauthorizedUserException e) {
+        log.error(e.getMessage());
+        return ApiResponse.error(HttpStatus.UNAUTHORIZED, UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(UserNotExistException.class)
+    public ApiResponse handleUserNotExistException(UserNotExistException e) {
+        log.error(e.getMessage());
+        return ApiResponse.error(HttpStatus.NOT_FOUND, USER_NOT_EXIST);
     }
 }
---
+++ src/main/java/com/daily/themoti/user/exception/UnauthorizedUserException.java
@@ -0,0 +1,8 @@
+package com.daily.themoti.user.exception;
+
+public class UnauthorizedUserException extends RuntimeException {
+
+    public UnauthorizedUserException(String message){
+        super(message);
+    }
+}
