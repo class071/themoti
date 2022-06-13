89d70aa5ff8bc7282c2c322efcd626afcfd5ba5d
we4sley <galaxy138742@gmail.com>
2022-06-14 00:40:01 +0900

    feat: 흡연구역 추가 메소드 컨트롤러 설정

--- src/main/java/com/daily/themoti/smokearea/controller/SmokeAreaController.java
+++ src/main/java/com/daily/themoti/smokearea/controller/SmokeAreaController.java
@@ -1,17 +1,16 @@
 package com.daily.themoti.smokearea.controller;
 
 import com.daily.themoti.global.api.ApiResponse;
+import com.daily.themoti.smokearea.dto.SaveAreaRequestDto;
 import com.daily.themoti.smokearea.dto.SmokeAreaResponseDto;
 import com.daily.themoti.smokearea.service.SmokeAreaService;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -31,5 +30,11 @@ public class SmokeAreaController {
     public ApiResponse<List<SmokeAreaResponseDto>> findByArea(@PathVariable long area){
         List<SmokeAreaResponseDto> dtos = smokeAreaService.findByArea(area);
         return ApiResponse.success(HttpStatus.OK, dtos);
+    }
+
+    @PostMapping("/add")
+    public ApiResponse<SmokeAreaResponseDto> addArea(@Valid @RequestBody SaveAreaRequestDto saveAreaRequestDto) {
+        SmokeAreaResponseDto smokeAreaResponseDto = smokeAreaService.addArea(saveAreaRequestDto);
+        return ApiResponse.success(HttpStatus.CREATED, smokeAreaResponseDto);
     }
 }
